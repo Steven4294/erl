615ed23ac51704db26f5e86cf4156217df6693ff
Steven Petteruti <stevenpetterutijr@gmail.cmo>
2019-02-22 13:09:37 -0500

    push

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const favicon = require('serve-favicon');
+const path = require('path');
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.get('/',function(req,res) {
   res.sendFile(__dirname + '/public/index.html');
